4839b74eb871536d22560ac8cac36397c445818e
Parisa <parisa7422@gmail.com>
2022-06-26 17:05:58 +0300

    Add calendar to the dashboard

--- client/src/components/Calender.js
+++ client/src/components/Calender.js
@@ -1,39 +1,71 @@
 import Wrapper from "../assets/wrappers/CalenderBox";
 
-const days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 const Calender = () => {
-  // First day of CURRENT MONTH
-  // function getFirstDayOfMonth(year, month) {
-  //   return new Date(year, month, 1);
-  // }
+  //First day of CURRENT MONTH
+  function getFirstDayOfMonth(year, month) {
+    return new Date(year, month, 1);
+  }
 
-  // const date = new Date();
-  // const firstDayCurrentMonth = getFirstDayOfMonth(
-  //   date.getFullYear(),
-  //   date.getMonth()
-  // );
+  const date = new Date();
 
-  // //  Last Day of CURRENT MONTH
-  // function getLastDayOfMonth(year, month) {
-  //   return new Date(year, month + 1, 0);
-  // }
+  const firstDayCurrentMonth = getFirstDayOfMonth(
+    date.getFullYear(),
+    date.getMonth()
+  );
 
-  // const lastDayCurrentMonth = getLastDayOfMonth(
-  //   date.getFullYear(),
-  //   date.getMonth()
-  // );
+  //  Last Day of CURRENT MONTH
+  function getLastDayOfMonth(year, month) {
+    return new Date(year, month + 1, 0);
+  }
 
+  const lastDayCurrentMonth = getLastDayOfMonth(
+    date.getFullYear(),
+    date.getMonth()
+  );
+
+  // get first day of the month as a string
+  // get number of days in per month as a number
+  const startTheWeek = String(firstDayCurrentMonth).slice(0, 3);
+  const daysOftheMonth = String(lastDayCurrentMonth).slice(8, 10);
+  const numberOfDays = parseInt(daysOftheMonth);
+
+  console.log("first day of week " + startTheWeek);
+  console.log("days of the month " + numberOfDays);
+
+  // Create the arry
+  const createArray = () => {
+    const array = [];
+    for (let i = 1; i <= numberOfDays; i++) {
+      array.push(i);
+    }
+    return array;
+  };
+  const monthArray = createArray();
+
+  // get Today date
+  const today = new Date().getDate();
+  console.log(today);
   return (
     <Wrapper>
-      <div className="days">
-        {days.map((day) => {
-          return (
-            <div className="day" key={day}>
-              {day}
-            </div>
-          );
-        })}
-      </div>
+      {days.map((day) => {
+        return (
+          <div className="day" key={day}>
+            {day}
+          </div>
+        );
+      })}
+      <div style={{ gridColumnStart: days.indexOf(startTheWeek) }}></div>
+      {monthArray.map((month, index) => {
+        return (
+          <div
+            key={month}
+            className={index + 1 === today ? "days select" : "days"}
+          >
+            {month}
+          </div>
+        );
+      })}
     </Wrapper>
   );
 };
